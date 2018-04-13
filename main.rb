b5fcb4b6306a4bb08638f6e282f6e1640e56451a
Brice <brice.bene@gmail.com>
2018-04-13 15:12:33 +0200

    identify match

--- main.rb
+++ main.rb
@@ -1,6 +1,31 @@
 require 'json'
+require 'byebug'
+require 'awesome_print'
+
+def order_filter(orders, direction = 'buy')
+  orders.select{|o| o['direction'] == direction}
+end
+
+def matcher(val1, val2)
+  val1.to_i == val2.to_i
+end
+
 
 data = JSON.parse(File.open('data.json').read)
 
-output_file = File.open('output.json', 'w')
-output_file.write()
+@users = data['users']
+@final_users = []
+queued_orders = data['queued_orders']
+
+@buying_orders = order_filter(queued_orders)
+@selling_orders = order_filter(queued_orders, 'sell')
+
+@orders = []
+@buying_orders.each do |buying_order|
+  @selling_orders.each do |selling_order|
+    if matcher(buying_order['price'], selling_order['price'])
+      #execute_queued_orders(buying_order, selling_order)
+      ap 'match'
+    end
+  end
+end
